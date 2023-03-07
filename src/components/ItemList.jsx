8c136be0f6956bdeb55649e76970bcfc1f023621
Fran-Driussi <frandriussi@gmail.com>
2023-03-07 16:33:39 -0300

    Cards

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,19 +1,45 @@
 import React from 'react'
 import Data from  "../data.json"
+import { ChakraProvider } from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, CardFooter, Stack,  Heading, Text, Divider, ButtonGroup, Button } from '@chakra-ui/react'
 
 const ItemList = () => {
   return (
+    <ChakraProvider>
     <div>
       {Data.map((prod)=>{
       return(
-        <div key={prod.id}>
-          <h2>{prod.name}</h2>
-          <p>{prod.precio}</p>
-          <p>Stock: {prod.stock}</p>
-        </div>
+        
+        <Card maxW='sm' key={prod.id}>
+  <CardBody>
+    <Stack mt='6' spacing='3'>
+      <Heading size='md'>{prod.name}</Heading>
+      <Text>
+        stock : {prod.stock}
+      </Text>
+      <Text color='blue.600' fontSize='2xl'>
+        {prod.precio}
+      </Text>
+    </Stack>
+  </CardBody>
+  <Divider />
+  <CardFooter>
+    <ButtonGroup spacing='2'>
+      <Button variant='solid' colorScheme='blue'>
+        Buy now
+      </Button>
+      <Button variant='ghost' colorScheme='blue'>
+        Add to cart
+      </Button>
+    </ButtonGroup>
+  </CardFooter>
+</Card>
+        
       )
     })}</div>
+    </ChakraProvider>
   )
+  
 }
 
 export default ItemList
